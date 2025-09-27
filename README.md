452097c3e3e837f8dac700ff9a27f9051d167990
diego.rosas <diegorosas19@gmail.com>
2025-09-27 09:22:01 -0300

    Iniciando o projeto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trabalho em Equipe
+
+## Atividade:
+Plataforma de cursos online EAD
+
+## Enunciado:
+
+### Classe e Métodos criados:
+
+### Resultado:
+
---
+++ app.js
@@ -0,0 +1,9 @@
+//Atividade (fases da Engenharia de Software)
+/*Equipe Escrum: Diego(Scrum Master)
+                 Kerolayne(Product Oner)
+                 Márcio
+                 Sotero
+*/ 
+//Projeto: Plataforma de cursos online EAD
+//Publico alvo: Voltados para auxiliar pessoas que precisam de interação com aplicativos de tecnologias.
+//Necessidades do cliente: Que seja fácil e intuitivo com priorização  para pessoas idosas.
